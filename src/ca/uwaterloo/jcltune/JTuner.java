9e277c42c192b4f7be88cb2a937041fe566d1352
Steven Stewart <steven.stewart@uwaterloo.ca>
2015-10-04 20:20:47 -0400

    JTuner initial commit

---
+++ src/ca/uwaterloo/jcltune/JTuner.java
@@ -0,0 +1,119 @@
+package ca.uwaterloo.jcltune;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * Wrapper class for CLTuner C++ Tuner object. Calls native functions. The dispose method should be
+ * called explicitly by the user when finished.
+ * 
+ * Please visit https://github.com/CNugteren/CLTune for more detailed documentation.
+ */
+public final class JTuner {
+
+  /*
+   * Loads the jcltuner native library.
+   * 
+   * "libjcltuner.so" for Linux. "libjcltuner.dylib" for Apple.
+   * 
+   * An UnsatisfiedLinkError will be thrown if the JVM cannot find the native library. Be sure to
+   * define java.library.path correctly.
+   * 
+   * For guidance: http://examples.javacodegeeks.com/java-basics/java-library-path
+   * -what-is-it-and-how-to-use/
+   */
+  static {
+    System.loadLibrary("jcltune");
+  }
+
+  /** A handle (memory address) to the C++ Tuner object. */
+  private long nativeHandle;
+
+  /**
+   * Creates a JTuner for the specified device on the specified platform.
+   * 
+   * @param platformId The ID for the OpenCL platform.
+   * @param deviceId The ID for the OpenCL device on the platform.
+   */
+  public JTuner(final int platformId, final int deviceId) {
+    init(platformId, deviceId);
+  }
+
+  @Override
+  public String toString() {
+    return "JTuner [nativeHandle=" + nativeHandle + "]";
+  }
+
+  /**
+   * Initialization; constructs the underlying C++ object.
+   * 
+   * @param platformId The ID for the OpenCL platform.
+   * @param deviceId The ID for the OpenCL device on the platform.
+   */
+  private native void init(int platformId, int deviceId);
+
+  /**
+   * Deletes the underlying C++ object. This method should be explicitly called when finished with
+   * the tuner.
+   */
+  public native void dispose();
+
+  public native int addKernel(List<String> filenames, String kernelName, int[] global, int[] local);
+
+  public native int addKernelFromString(String source, String kernelName, int[] global, int[] local);
+
+  public native void setReference(List<String> filenames, String kernelName, int[] global,
+      int[] local);
+
+  public native void setReferenceFromString(String source, String kernelName, long[] global,
+      long[] local);
+
+  public native void addParameter(int id, String parameterName, int[] values);
+
+  public native void addParameterReference(String parameterName, int value);
+
+  public native void mulGlobalSize(int id, String[] range);
+
+  public native void divGlobalSize(int id, String[] range);
+
+  public native void mulLocalSize(int id, String[] range);
+
+  public native void divLocalSize(int id, String[] range);
+
+  public native void addConstraint(int id, Function<List<Integer>, Boolean> validIf,
+      List<String> parameters);
+
+  public native void setLocalMemoryUsage(int id, Function<List<Integer>, Integer> amount,
+      List<String> parameters);
+
+  public native <T> void addArgumentInput(List<T> source);
+
+  public native <T> void addArgumentOutput(List<T> source);
+
+  public native <T> void addArgumentScalar(T argument);
+
+  public native void useFullSearch();
+
+  public native void useRandomSearch(double fraction);
+
+  public native void useAnnealing(double fraction, double maxTemperature);
+
+  public native void usePSO(double fraction, int swarmSize, double influenceGlobal,
+      double influenceLocal, double influenceRandom);
+
+  public native void outputSearchLog(String filename);
+
+  public native void tune();
+
+  public native double printToScreen();
+
+  public native void printFormatted();
+
+  public native void printJSON(String filename, Map<String, String> descriptions);
+
+  public native void printToFile(String filename);
+
+  public native void suppressOutput();
+
+}
---
+++ src/ca/uwaterloo/jcltune/Main.java
@@ -0,0 +1,45 @@
+package ca.uwaterloo.jcltune;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+  public static void main(String[] args) {
+    final int platformId;
+    final int deviceId;
+
+    // Check if platformId and deviceId were passed as arguments
+    if (args.length == 2) {
+      platformId = Integer.parseInt(args[0]);
+      deviceId = Integer.parseInt(args[1]);
+    } else {
+      platformId = 0;
+      deviceId = 0;
+    }
+
+    // Constants
+    final int N = 1024; // host array size
+    final int WGSZ = 512; // workgroup (local) size
+
+    // Host data
+    int[] A = new int[N];
+    int[] B = new int[N];
+
+    // Set up tuner
+    JTuner myTuner = new JTuner(platformId, deviceId);
+    final int kernelId =
+        myTuner.addKernel(new ArrayList<String>(Arrays.asList("hello.cl")), "hello", new int[] {N},
+            new int[] {WGSZ});
+
+    // Add tuning parameters
+    myTuner.addParameter(kernelId, "ABC", new int[] {32, 64});
+
+    // Set reference
+    myTuner.setReference(new ArrayList<String>(Arrays.asList("hello.cl")), "hello", new int[] {N},
+        new int[] {WGSZ});
+
+    // Deletes the native C++ object
+    myTuner.dispose();
+  }
+}
